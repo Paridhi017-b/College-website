b76a325bdacc2b499c3ff581f2a692692a505877
Paridhi017-b <128114698+Paridhi017-b@users.noreply.github.com>
2024-06-29 15:20:01 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,181 @@
+//1Function to open the modal
+function openModal() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("myImage");
+    var modalImg = document.getElementById("modalImg");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+// Function to close the modal
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+//2Function to open the modal
+function penModal() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("byImage");
+    var modalImg = document.getElementById("modalImg");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+// Function to close the modal
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+//3 Function to open the modal
+function whenModal() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("whyImage");
+    var modalImg = document.getElementById("modalImg");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+// Function to close the modal
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+// 4Function to open the modal
+function whoModal() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("hiiImage");
+    var modalImg = document.getElementById("modalImg");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+// Function to close the modal
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+// 5Function to open the modal
+function createModal() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("editImage");
+    var modalImg = document.getElementById("modalImg");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+// Function to close the modal
+function myModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+// Function to open the modal
+function helloModal() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("clrImage");
+    var modalImg = document.getElementById("modalImg");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+// Function to close the modal
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+// Function to open the modal
+function clrModal() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("dateImage");
+    var modalImg = document.getElementById("modalImg");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+// Function to close the modal
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+// Function to open the modal
+function setModal() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("setImage");
+    var modalImg = document.getElementById("modalImg");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+// Function to close the modal
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+// Function to open the modal
+function codeModal() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("codeImage");
+    var modalImg = document.getElementById("modalImg");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+// Function to close the modal
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+// Function to open the modal
+function acceptModal() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("acceptImage");
+    var modalImg = document.getElementById("modalImg");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+// Function to close the modal
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+// Function to open the modal
+function goodModal() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("goodImage");
+    var modalImg = document.getElementById("modalImg");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+// Function to close the modal
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+// Function to open the modal
+function aveModal() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("aveImage");
+    var modalImg = document.getElementById("modalImg");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+// Function to close the modal
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+
